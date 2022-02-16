e66aa680b4a6059a042fe579f638d06385568419
nepp.plus <nepp.plus@gmail.com>
2022-02-16 11:47:50 +0900

    플레이 스토어로 바로 링크

--- app/src/main/java/com/nepplus/androidintent_20220216/MainActivity.kt
+++ app/src/main/java/com/nepplus/androidintent_20220216/MainActivity.kt
@@ -55,5 +55,13 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        btnKakaoTalkPlayStoreLink.setOnClickListener {
+
+            val myUri = Uri.parse("market://details?id=com.kakao.talk")
+            val myIntent = Intent( Intent.ACTION_VIEW, myUri )
+            startActivity(myIntent)
+
+        }
+
     }
 }
